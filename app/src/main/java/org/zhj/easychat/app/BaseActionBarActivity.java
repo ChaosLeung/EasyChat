bc6925a49c2a8080146a02de7b49ea76171f612d
ChaosLeong <lgf42031@gmail.com>
2015-06-20 22:14:08 +0800

    项目Close

--- app/src/main/java/org/zhj/easychat/app/BaseActionBarActivity.java
+++ app/src/main/java/org/zhj/easychat/app/BaseActionBarActivity.java
@@ -1,13 +1,14 @@
 package org.zhj.easychat.app;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 /**
  * @author Chaos
  *         2015/02/24.
  */
-public class BaseActionBarActivity extends ActionBarActivity {
+public class BaseActionBarActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
